b6e7eaf3d4aceda7e684d3dd3a3cdf97bbd47089
andreaswestrell <andreas.westrell@gmail.com>
2021-12-10 22:30:10 +0000

    Cleanup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # debian
+My *taliored* post-install script for debian
+
+* Changes branch to SID
+	* Updates packages 
+	* Full-upgrade
+
+* Installations 
+	* Gnome software and it's flatpak plugin
+	* The inter font and reloads the font cache
+	* The nvidia-driver
+	* Flatpak
+		* Adds the Flathub remote
+		* Github desktop for linux
--- root.sh
+++ root.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# Ensures this runs as root
+sudo -s
 # Change Debian to SID Branch
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp sources.list /etc/apt/sources.list
-
 # Update packages
 apt update -y
 apt full-upgrade -y 
-
+## Installations
+# Installs flatpak
+apt install flatpak -y
+# Adds flathub remote
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# Gnome software and it's flakpak plugin
+apt install gnome-software gnome-software-plugin-flatpak -y
 # inter font
-apt install fonts-inter-variable
+apt install fonts-inter-variable -y
+# Font cache
+fc-cache -vf
+# Nvidia driver
+apt install nvidia-driver -y
+## Flatpaks
+flatpak install flathub io.github.shiftey.Desktop
